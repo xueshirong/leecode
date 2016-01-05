5e1168102febca09925b998eccaf050b551e7f51
xueshirong <xueshirong@gmail.com>
2016-01-04 23:34:37 -0600

    Maximum_depth_of_Binary_TreeNode

---
+++ src/org/leecode/Maximum_depth_of_Binary_TreeNode.java
@@ -0,0 +1,10 @@
+package org.leecode;
+
+public class Maximum_depth_of_Binary_TreeNode {
+	int val;
+	Maximum_depth_of_Binary_TreeNode left;
+	Maximum_depth_of_Binary_TreeNode right;
+	Maximum_depth_of_Binary_TreeNode(int x) {
+		val = x; 
+	}
+}
