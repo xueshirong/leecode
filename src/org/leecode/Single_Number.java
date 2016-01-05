9416922a45757e5fa56a3e92b89dfd7ac338aa09
xueshirong <xueshirong@gmail.com>
2016-01-05 00:32:43 -0600

    Single_Number

---
+++ src/org/leecode/Single_Number.java
@@ -0,0 +1,27 @@
+package org.leecode;
+
+import java.awt.List;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Single_Number {
+	public int singleNumber1(int[] nums) {
+        HashSet hs = new HashSet();
+        for(int i = 0; i < nums.length; i++){
+            if (hs.contains(nums[i]))
+                hs.remove(nums[i]);
+            else
+                hs.add(nums[i]);
+        }
+        return Integer.parseInt(hs.iterator().next().toString());
+    }
+	
+	public int singleNumber(int[] nums) {
+		//a^a^b^b^c = (a^a)^(b^b)^c=0^0^c=c
+		int ini = 0;
+        for (int num : nums){
+        	ini = ini ^ num;
+        }
+        return ini;
+    }
+}
