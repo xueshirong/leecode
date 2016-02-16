88566a3b3b655bcd254936af7a554fd206455af7
xueshirong <xueshirong@gmail.com>
2016-02-15 18:18:38 -0600

    new

---
+++ src/org/leecode/Test/Reverse_Linked_ListTest.java
@@ -0,0 +1,31 @@
+package org.leecode.Test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.leecode.ListNode;
+import org.leecode.Reverse_Linked_List;
+import org.leecode.Common.Utility;
+
+public class Reverse_Linked_ListTest {
+	Reverse_Linked_List rll = new Reverse_Linked_List();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testReverseList() {
+		ListNode head = Utility.getTestListNode();
+		/*while(head != null){
+			System.out.println(head.val);
+			head = head.next;
+		}*/
+		ListNode newHead = rll.reverseList(head);
+		while(newHead != null){
+			System.out.println(newHead.val);
+			newHead = newHead.next;
+		}
+	}
+
+}
