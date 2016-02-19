736df214d50cad16f9637d14bf6a651f72537809
xueshirong <xueshirong@gmail.com>
2016-02-19 13:02:52 -0600

    update

--- src/org/leecode/Delete_Node_in_a_Linked_List_ListNode.java
+++ src/org/leecode/Delete_Node_in_a_Linked_List_ListNode.java
@@ -3,7 +3,7 @@ package org.leecode;
 public class Delete_Node_in_a_Linked_List_ListNode {
 	public int val;
 	public Delete_Node_in_a_Linked_List_ListNode next;
-	Delete_Node_in_a_Linked_List_ListNode(int x) { 
+	public Delete_Node_in_a_Linked_List_ListNode(int x) { 
 		val = x;
 	}
 }
