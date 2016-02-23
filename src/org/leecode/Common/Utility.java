740d49b27169f9f45bf6eec036fe2a6223877c6c
xueshirong <xueshirong@gmail.com>
2016-02-23 17:47:38 -0600

    update

--- src/org/leecode/Common/Utility.java
+++ src/org/leecode/Common/Utility.java
@@ -42,4 +42,24 @@ public class Utility {
 		H.next = F;
 		return A;
 	}
+
+	public static ListNode getTestDuplicatesListNode(){
+		ListNode A = new ListNode(1);
+		ListNode B = new ListNode(1);
+		ListNode C = new ListNode(2);
+		/*ListNode D = new ListNode(4);
+		ListNode E = new ListNode(5);
+		ListNode F = new ListNode(6);
+		ListNode G = new ListNode(7);
+		ListNode H = new ListNode(8);*/
+		
+		A.next = B;
+		B.next = C;
+		/*C.next = D;
+		D.next = E;
+		E.next = F;
+		F.next = G;
+		G.next = H;*/
+		return A;
+	}
 }
