de89204876b7c2ddd6f316f679ef7c36d3d4d0f9
xueshirong <xueshirong@gmail.com>
2016-03-19 14:53:15 -0500

    new

---
+++ src/org/leecode/Test/Rotate_ListTest.java
@@ -0,0 +1,23 @@
+package org.leecode.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.leecode.Rotate_List;
+import org.leecode.Common.ListNode;
+import org.leecode.Common.Utility;
+
+public class Rotate_ListTest {
+	Rotate_List rl = new Rotate_List();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRotateRight() {
+		ListNode head = Utility.getTestListNode();
+		rl.rotateRight(head, 2);
+	}
+
+}
