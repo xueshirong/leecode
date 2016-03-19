0a39646a94672d5db641abdd2039a90230e5ad34
xueshirong <xueshirong@gmail.com>
2016-03-19 16:49:06 -0500

    add

---
+++ src/org/leecode/Move_Zeroes2.java
@@ -0,0 +1,17 @@
+package org.leecode;
+
+public class Move_Zeroes2 {
+	public int[] moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++){//move all non-negative to front
+        	if (nums[i] != 0){
+            	Integer tem = nums[i];
+            	nums[i] = nums[j];
+            	nums[j] = tem;
+                j++;
+            }
+        }
+         
+        return nums;
+	}
+}
