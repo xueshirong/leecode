023cb345a87aaec9fc4515a2a35ec58e472c4673
xueshirong <xueshirong@gmail.com>
2016-03-20 01:23:47 -0500

    new

---
+++ src/org/leecode/Test/Contains_Duplicate_IITest.java
@@ -0,0 +1,22 @@
+package org.leecode.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.leecode.Contains_Duplicate_II;
+
+public class Contains_Duplicate_IITest {
+	Contains_Duplicate_II cdii = new Contains_Duplicate_II();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testContainsNearbyDuplicate() {
+		int[] nums = new int[]{4,1,2,3,1,5};
+		cdii.containsNearbyDuplicate(nums, 3);
+		
+	}
+
+}
