efce516713cc0a69a3bb37be4f3da951a6847a73
xueshirong <xueshirong@gmail.com>
2016-03-26 19:48:39 -0500

    new

---
+++ src/org/leecode/Test/Product_of_Array_Except_SelfTest.java
@@ -0,0 +1,21 @@
+package org.leecode.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.leecode.Product_of_Array_Except_Self;
+
+public class Product_of_Array_Except_SelfTest {
+	Product_of_Array_Except_Self pae = new Product_of_Array_Except_Self();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testProductExceptSelf() {
+		int[] nums = new int[]{0,0};
+		pae.productExceptSelf(nums);
+	}
+
+}
---
+++ src/org/leecode/Test/Two_SumTest.java
@@ -0,0 +1,26 @@
+package org.leecode.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.leecode.Two_Sum;
+
+public class Two_SumTest {
+	Two_Sum ts = new Two_Sum();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTwoSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTwoSum2() {
+		int[] nums = new int[]{3,2,4};
+		ts.twoSum2(nums, 6);
+	}
+
+}
