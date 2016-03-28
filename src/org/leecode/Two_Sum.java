794bef3e2b77548d5e3c9ba007d2f4626012c1a0
xueshirong <cse8243@cse118243.uta.edu>
2016-03-28 13:56:13 -0500

    uy

---
+++ src/org/leecode/Two_Sum.java
@@ -0,0 +1,33 @@
+package org.leecode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two_Sum {
+	public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] ==  target){
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+	public int[] twoSum2(int[] nums, int target) {
+		int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target - nums[i])){
+                result[0] = map.get(nums[i]);
+                result[1] = i;
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
