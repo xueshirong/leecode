1cbf790fedcd35c1f0ddf401f96f280631cb2142
xueshirong <xueshirong@gmail.com>
2016-03-30 18:03:59 -0500

    update

--- src/org/leecode/Find_Minimum_in_Rotated_Sorted_Array.java
+++ src/org/leecode/Find_Minimum_in_Rotated_Sorted_Array.java
@@ -1,6 +1,10 @@
 package org.leecode;
 
 public class Find_Minimum_in_Rotated_Sorted_Array {
+	/*Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+	(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+	Find the minimum element.
+	You may assume no duplicate exists in the array.*/
 	public int findMin(int[] nums) {
 		int start = 0;
         int end = nums.length - 1;
