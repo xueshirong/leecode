b6e8357492b0a71e46f0f9b14fcf220ce313af7b
xueshirong <xueshirong@gmail.com>
2016-04-17 00:48:15 -0500

    update

--- src/JobInterview/Count24Game.java
+++ src/JobInterview/Count24Game.java
@@ -1,122 +1,143 @@
 package JobInterview;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Count24Game {
-    private List<String> answerList=new ArrayList<String>();
+	private static int target;	
+    private List<String> answerList = new ArrayList<String>();
     public List<String> getAnswerList() {
         return answerList;
     }
-    public static class Data{
+    public class Data{
         public float[]  arr;
-        public String expStr="";
+        public String expStr = "";
         public String[] strs;
         public Data(){}
-        public Data(int a,int b,int c,int d) {
-            arr=new float[]{a,b,c,d};
-            strs=new String[]{a+"",b+"",c+"",d+""};
-            expStr=a+"";
-        }
         public Data(int arr[]) {
         	this.arr = new float[arr.length];
         	this.strs = new String[arr.length];
         	for (int i = 0; i < arr.length; i++){
         		this.arr[i] = Float.valueOf(arr[i]);
         		this.strs[i] = String.valueOf(arr[i]);
-        		/*this.arr = new float[]{arr[0],arr[1],arr[2],arr[3]};
-        		this.strs = new String[]{arr[0]+"",arr[1]+"",arr[2]+"",arr[3]+""};*/
         	}
         }
     }
     public void count(Data data){
-        float[] arr=data.arr;
-        if(arr.length<=1){
-            if(arr.length==1&&arr[0]==24){
-                answerList.add(data.expStr.substring(1, data.expStr.length()-1));
+        float[] arr = data.arr;
+        if(arr.length <= 1){
+            if(arr.length == 1 && arr[0] == Count24Game.target){
+                answerList.add(data.expStr.substring(1, data.expStr.length() - 1));
             }
-            return ;
+            return;
         }
-        for(int i=0,len=arr.length;i<len-1; i++){
-            for(int j=i+1;j<len;j++){
-                float x=arr[i];
-                float y=arr[j];
-                String xs=data.strs[i];
-                String ys=data.strs[j];
-                for(int k=0;k<6;k++){
-                    Data newData=getNewArr(data,i);
+        for(int i = 0,len = arr.length;i < len - 1; i++){
+            for(int j = i+1; j < len; j++){
+                float x = arr[i];
+                float y = arr[j];
+                String xs = data.strs[i];
+                String ys = data.strs[j];
+                for(int k = 0; k < 6; k++){
+                    Data newData = getNewArr(data,i);
                     switch(k){
                         case 0:
-                        newData.arr[j-1]=x+y;
+                        newData.arr[j-1] = x + y;
                             newData.expStr=xs+"+"+ys;
                         break;
                         case 1:
-                        newData.arr[j-1]=x-y;
+                        newData.arr[j-1] = x - y;
                             newData.expStr=xs+"-"+ys;
                         break;
                         case 2:
-                        newData.arr[j-1]=y-x;
+                        newData.arr[j-1] = y - x;
                         newData.expStr=ys+"-"+xs;
                         break;
                         case 3:
-                        newData.arr[j-1]=x*y;
-                            newData.expStr=xs+"*"+ys;
+                        newData.arr[j-1] = x * y;
+                            newData.expStr = xs + "*" + ys;
                         break;
                         case 4:
                         if(y!=0){
-                            newData.arr[j-1]=x/y;
-                                newData.expStr=xs+"/"+ys;
+                            newData.arr[j-1] = x/y;
+                                newData.expStr = xs + "/" + ys;
                         }else {
                             continue;
                         }
                         break;
                         case 5:
-                        if(x!=0){
-                            newData.arr[j-1]=y/x;
-                                newData.expStr=ys+"/"+xs;
+                        if(x != 0){
+                            newData.arr[j-1] = y/x;
+                                newData.expStr = ys+"/"+xs;
                         }else {
                             continue;
                         }
                         break;
                     }
-                    newData.expStr="("+newData.expStr+")";
-                    newData.strs[j-1]=newData.expStr;
+                    newData.expStr = "(" + newData.expStr + ")";
+                    newData.strs[j-1] = newData.expStr;
                     count(newData);
                 }
             }
-        }
-        
+        }        
     }
-    private static Data getNewArr(Data data, int i) {
-        Data newData=new Data();
-        newData.expStr=data.expStr;
-        newData.arr=new float[data.arr.length-1];
-        newData.strs=new String[data.arr.length-1];
-        for(int m=0,len=data.arr.length,n=0;m<len;m++){
-            if(m!=i){
-                newData.arr[n]=data.arr[m];
-                newData.strs[n]=data.strs[m];
+    private Data getNewArr(Data data, int i) {
+        Data newData = new Data();
+        newData.expStr = data.expStr;
+        newData.arr = new float[data.arr.length-1];
+        newData.strs = new String[data.arr.length-1];
+        for(int m = 0, len = data.arr.length,n = 0; m < len; m++){
+            if(m != i){
+                newData.arr[n] = data.arr[m];
+                newData.strs[n] = data.strs[m];
                 n++;
             }
         }
         return newData;
     }
     
-    public static final List<String> easyCount(int[] curRandNums){
-        Count24Game count24=new Count24Game();
-        count24.count(new Data(curRandNums));
-        Set<String> set=new HashSet<String>(count24.getAnswerList());//remove duplicated set
+    public List<String> easyCount(int[] inputValues){
+        Count24Game count24 = new Count24Game();
+        count24.count(new Data(inputValues));
+        Set<String> set = new HashSet<String>(count24.getAnswerList());//remove duplicated set
         return new ArrayList<String>(set);
     }
     
-    public static void main(String[] args) throws InterruptedException {
-        long start=System.currentTimeMillis();
-        List<String> answerStris=easyCount(new int[]{2,3,4});
-        System.out.println(System.currentTimeMillis()-start);
+    public static void main(String[] args) {
+    	Count24Game count24 = new Count24Game();
+    	int[] inputValues = new int[]{};
+    	try{
+    		inputValues = count24.getInputValues();    		
 
-        for (String string : answerStris) {
-            System.out.println(string);
-        }
+        	List<String> answerStris= count24.easyCount(inputValues);
+        	if (answerStris.size() == 0){
+        		System.out.println("none");
+        	}else{
+        		for (String string : answerStris) {
+        			System.out.println(string);
+        		}
+        	}
+        	return;
+    	}catch(Exception ex){
+    		System.out.println("Input values are invalid!");
+    	}    	
     }
+	private int[] getInputValues() throws InterruptedException, IOException, NumberFormatException {
+		System.out.println("Input values(please use white space to seperate them): ");
+  	  
+    	BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+    	String inputString = bufferRead.readLine();
+    	
+    	String[] inputArr = inputString.split(" ");
+    			
+    	target = Integer.valueOf(inputArr[0]);
+    	int[] inputValues = new int[inputArr.length - 1];
+    	for(int i = 1; i < inputArr.length; i++){
+    		inputValues[i - 1] = Integer.valueOf(inputArr[i]);
+    	}
+    	return inputValues;
+	}
 }
