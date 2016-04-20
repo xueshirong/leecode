163c0127842d4687dc1bdde6e9a92492efd8a5b7
xueshirong <xueshirong@gmail.com>
2016-04-19 22:41:07 -0500

    update

--- src/org/leecode/Remove_Duplicates_from_Sorted_Array_II.java
+++ src/org/leecode/Remove_Duplicates_from_Sorted_Array_II.java
@@ -1,5 +1,8 @@
 package org.leecode;
-
+/*
+ * 当A[end-1] = A[end] = A[i]时，A[i]为重复元素需跳过。而实际只需要比较A[end-1]和A[i]，
+ * 因为当A[end-1] = A[i]时，根据sorted arry特性必然也有A[end]=A[end-1]。
+*/
 public class Remove_Duplicates_from_Sorted_Array_II {
 	public int removeDuplicates(int[] nums) {
         if (nums.length < 3)
