caceda99e71a84145bfb6aa67d1c6919776d1842
xueshr <xueshirong@gmail.com>
2016-05-11 17:22:01 -0500

    new

---
+++ src/org/leecode/Path_Sum.java
@@ -0,0 +1,18 @@
+package org.leecode;
+
+public class Path_Sum {
+	public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null){
+            return false;
+        }
+        if (root.val == sum && root.left == null && root.right == null){
+            return true;
+        }
+        
+        if (hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)){
+            return true;
+        }
+        
+        return false;
+    }
+}
