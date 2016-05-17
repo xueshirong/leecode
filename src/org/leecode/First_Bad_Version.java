6d45b60d9c253e3b3c0cf7347dc5e574c183fde0
Xue <Xue@10.182.84.145>
2016-05-17 13:02:50 -0500

    new

---
+++ src/org/leecode/First_Bad_Version.java
@@ -0,0 +1,24 @@
+package org.leecode;
+
+public class First_Bad_Version {
+	public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while(start < end){
+        	int mid = start + (end - start)/2; //this line is a tricky
+            if (isBadVersion(mid)){
+                end = mid;
+                
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+	
+	boolean isBadVersion(int n){
+		if (n >= 4)
+			return true;
+		return false;
+	}
+}
---
+++ src/org/leecode/Test/First_Bad_VersionTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.First_Bad_Version;
+
+public class First_Bad_VersionTest {
+	First_Bad_Version p = new First_Bad_Version();
+	@Test
+	public void testFirstBadVersion() {
+		p.firstBadVersion(4);
+	}
+
+}
