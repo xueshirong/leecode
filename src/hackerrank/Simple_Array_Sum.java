30bf0be57a15dcb3034510342b537d8883641fa1
Xue <Xue@129.107.210.176>
2016-05-25 00:01:52 -0500

    new

---
+++ src/hackerrank/Simple_Array_Sum.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Simple_Array_Sum {
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+		Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();        
+        int sum = 0;
+        for (int i = 0; i < size; i++){
+            sum = sum + scan.nextInt();
+        }		
+        System.out.println(sum);
+    }
+}
