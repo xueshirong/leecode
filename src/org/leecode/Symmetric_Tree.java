ab2ef33fc29176862cdbdab101cf8532927ad02a
xueshr <xueshirong@gmail.com>
2016-06-01 00:01:45 -0500

    update

--- src/org/leecode/Symmetric_Tree.java
+++ src/org/leecode/Symmetric_Tree.java
@@ -1,8 +1,12 @@
 package org.leecode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Symmetric_Tree {
 	public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
+        
         return helper(root.left, root.right);
     }
     boolean helper(TreeNode left, TreeNode right){
@@ -15,6 +19,28 @@ public class Symmetric_Tree {
         boolean con1 = (left.val == right .val);
         boolean con2 = helper(left.left, right.right);
         boolean con3 = helper(left.right, right.left);
-        return con1 && con2 && con3;
+        return con1 && con2 && con3;                
+    }
+    
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null) return true;
+        Queue<TreeNode> q1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> q2 = new LinkedList<TreeNode>();
+        q1.offer(root.left);
+        q2.offer(root.right);
+        while (!q1.isEmpty() && !q2.isEmpty()){
+        	TreeNode n1 = q1.poll();
+        	TreeNode n2 = q2.poll();
+        	if (n1 == null && n2 == null)
+        		continue;
+        	if (n1 == null || n2 == null || n1.val != n2.val)
+        		return false;
+        	q1.offer(n1.left);
+        	q2.offer(n2.right);
+        	q1.offer(n1.right);
+        	q2.offer(n2.left);
+        }
+        
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
