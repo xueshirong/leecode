b3a5c4ee47f58084eaa67f78441d9e7fac785528
Xue <Xue@tomy>
2016-06-02 15:17:37 -0500

    new

---
+++ src/org/leecode/Power_of_Four.java
@@ -0,0 +1,16 @@
+package org.leecode;
+
+public class Power_of_Four {
+	public boolean isPowerOfFour(int num) {
+        if (num <= 0)
+            return false;
+        if (num == 1)
+            return true;
+        while(num > 1){
+            if (num % 4 != 0)
+                return false;
+            num = num / 4;
+        }
+        return true;
+    }
+}
