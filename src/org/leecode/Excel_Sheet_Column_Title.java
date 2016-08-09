12eb60f48a9afaa209358c7192ff55d131d70563
Xue <Xue@tomy>
2016-08-08 22:19:50 -0500

    new

---
+++ src/org/leecode/Excel_Sheet_Column_Title.java
@@ -0,0 +1,13 @@
+package org.leecode;
+
+public class Excel_Sheet_Column_Title {
+	public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n > 0){
+            n--;
+            sb.insert(0, (char)('A' + n % 26));
+            n = n / 26;
+        }
+        return sb.toString();
+    }
+}
---
+++ src/org/leecode/Test/Excel_Sheet_Column_TitleTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Excel_Sheet_Column_Title;
+
+public class Excel_Sheet_Column_TitleTest {
+	Excel_Sheet_Column_Title p = new Excel_Sheet_Column_Title();
+	@Test
+	public void testConvertToTitle() {
+		p.convertToTitle(1);
+	}
+
+}
