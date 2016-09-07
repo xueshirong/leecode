e44d9aa409c0c853a81871146c1b64c0afdae858
Xue <Xue@10.182.187.22>
2016-09-06 21:00:15 -0500

    update

--- src/org/leecode/Reverse_Linked_List.java
+++ src/org/leecode/Reverse_Linked_List.java
@@ -15,4 +15,16 @@ public class Reverse_Linked_List {
 		
 		return pre;
 	}
+	public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode pre = null;
+        while (head != null){
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
 }
