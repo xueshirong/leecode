263415bf448da05e8d5e70b7fca08a8620b91048
Xue <Xue@tomy>
2016-09-08 00:36:39 -0500

    update

--- src/org/leecode/Reverse_Linked_List_II.java
+++ src/org/leecode/Reverse_Linked_List_II.java
@@ -14,14 +14,35 @@ public class Reverse_Linked_List_II {
             pre = pre.next;
         }
         ListNode nodeM = pre.next;
-        ListNode next = nodeM.next;
+        ListNode temp = nodeM.next;
         for (int i = 0; i < n-m; i++){
-        	nodeM.next = next.next;
-        	next.next = pre.next;
-        	pre.next = next;
-        	next = nodeM.next;
+        	nodeM.next = temp.next;
+        	temp.next = pre.next;
+        	pre.next = temp;
+        	temp = nodeM.next;
         }
         return dumpHead.next;
     }
-    
+	public ListNode reverseBetween2(ListNode head, int m, int n) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode dump = new ListNode(0);
+        dump.next = head;
+        ListNode pre = dump;
+        
+        int index = 1;
+        while(pre != null && index < m){
+            pre = pre.next;
+            index++;
+        }
+        ListNode nodeM = pre.next;
+        for (int i = m; i < n; i++){
+            ListNode temp = nodeM.next;
+            nodeM.next = temp.next;
+            temp.next = pre.next;
+            pre.next =temp;
+        }
+        
+        return dump.next;
+    }
 }
--- src/org/leecode/Test/Reverse_Linked_List_IITest.java
+++ src/org/leecode/Test/Reverse_Linked_List_IITest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.leecode.Reverse_Linked_List_II;
 import org.leecode.Common.ListNode;
-import org.leecode.Common.Utility;
 
 public class Reverse_Linked_List_IITest {
 	Reverse_Linked_List_II rllii = new Reverse_Linked_List_II();
@@ -14,7 +13,12 @@ public class Reverse_Linked_List_IITest {
 
 	@Test
 	public void testReverseBetween() {
-		ListNode head = Utility.getTestListNode();
+		//ListNode head = Utility.getTestListNode();
+		ListNode head = new ListNode(1);
+		ListNode head1 = new ListNode(2);
+		ListNode head2 = new ListNode(3);
+		head.next = head1;
+		head1.next = head2;
 		rllii.reverseBetween(head, 1, 3);
 	}
 
