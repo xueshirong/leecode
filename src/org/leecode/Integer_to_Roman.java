71aa7f1312b878bdb8f50aedf9f0d7cfa521a0a2
Xue <Xue@10.182.163.79>
2016-09-11 21:36:44 -0500

    new

---
+++ src/org/leecode/Integer_to_Roman.java
@@ -0,0 +1,19 @@
+package org.leecode;
+
+public class Integer_to_Roman {
+	 public String intToRoman(int num) {
+	        //it is important to know all the special numer 1,4, 5, 9
+	        String[] symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	        int[] nums = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+	        
+	        String res = "";
+	        for (int i = 0; i < nums.length; i++){
+	            int times = num/nums[i];//calclate how many times for each symbol
+	            num -= times * nums[i];//get the new num
+	            for (int j = 0; j < times; j++){
+	                res += symbol[i];
+	            }
+	        }
+	        return res;
+	 }
+}
