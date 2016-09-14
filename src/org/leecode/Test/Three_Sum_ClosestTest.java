9e02c12ac0d32980dd1e83292e3906f74d2bea01
Xue <Xue@tomy>
2016-09-14 00:36:53 -0500

    update

--- src/org/leecode/Test/Three_Sum_ClosestTest.java
+++ src/org/leecode/Test/Three_Sum_ClosestTest.java
@@ -7,8 +7,8 @@ public class Three_Sum_ClosestTest {
 	Three_Sum_Closest p = new Three_Sum_Closest();
 	@Test
 	public void testThreeSumClosest() {
-		int[] nums = new int[]{-3,-2,-5,3,-4};
-		p.threeSumClosest(nums, -1);
+		int[] nums = new int[]{1,1,1,0};
+		p.threeSumClosest3(nums, 100);
 	}
 
 }
--- src/org/leecode/Three_Sum_Closest.java
+++ src/org/leecode/Three_Sum_Closest.java
@@ -62,4 +62,31 @@ public class Three_Sum_Closest {
         }
         return sum;
     }
+	
+	public int threeSumClosest3(int[] nums, int target) {
+		if (nums == null || nums.length < 3)
+            return -1;
+        
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[2];//base line value
+        for(int i = 0; i < nums.length; i++){
+            //if (i != 0 && nums[i] == nums[i - 1])
+            //    continue;
+            int l = i + 1;
+            int r = nums.length - 1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == target){
+                    return sum;
+                }else if (sum > target){
+                    r--;                    
+                }else{
+                    l++;                    
+                }
+                if (Math.abs(target - sum) < Math.abs(target - res))//compare current sum with base line
+                	res = sum;
+            }
+        }
+        return res;
+    }
 }
