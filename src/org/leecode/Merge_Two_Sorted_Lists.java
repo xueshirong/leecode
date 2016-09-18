1896b2e552dc90762c7c00c014aecc8bd7dac3ad
Xue <Xue@10.182.26.200>
2016-09-17 23:13:33 -0500

    update

--- src/org/leecode/Merge_Two_Sorted_Lists.java
+++ src/org/leecode/Merge_Two_Sorted_Lists.java
@@ -28,4 +28,25 @@ public class Merge_Two_Sorted_Lists {
         
         return head.next;
 	}
+	//From Jiu Zhang
+	public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while(l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                head.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        if (l1 != null)
+            head.next = l1;
+        else
+            head.next = l2;
+        return dummy.next;
+    }
 }
