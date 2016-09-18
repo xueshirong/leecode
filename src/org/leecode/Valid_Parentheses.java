43bd820530d1cf647594f00d6df0209ae0ebf64a
Xue <Xue@10.182.26.200>
2016-09-17 22:50:19 -0500

    new

---
+++ src/org/leecode/Valid_Parentheses.java
@@ -0,0 +1,30 @@
+package org.leecode;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+	//From Jiu Zhang
+	public boolean isValid(String s) {
+        if(s == "")
+            return false;
+        String target = "([{";
+        Stack<Character> stack = new Stack<Character>();
+        for(Character c: s.toCharArray()){
+            if (target.contains(String.valueOf(c)))//if c is left Parentheses
+                stack.push(c);//add c to stack
+            else{//if c is right Parentheses, check the peek is pair with c or not
+                if (!stack.isEmpty() && isPair(stack.peek(), c))//if c's pair is in the stack peek, then pop the pair
+                    stack.pop();
+                else
+                    return false;
+            }
+        }//finally, if string is valid, the stack should be empty
+        return stack.isEmpty();
+    }
+    //check c1 and c2 is pair or not
+    private boolean isPair(Character c1, Character c2){
+        if ((c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}'))
+            return true;
+        return false;
+    }
+}
