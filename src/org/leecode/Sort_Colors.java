3d6ada1c12bb43647960af6be6bb32953d75baf7
Xue <Xue@tomy>
2016-09-18 17:43:29 -0500

    new

---
+++ src/org/leecode/Sort_Colors.java
@@ -0,0 +1,30 @@
+package org.leecode;
+
+public class Sort_Colors {
+	//From Jiu Zhang
+	public void sortColors(int[] nums) {
+        if (nums == null || nums.length < 2)
+            return;
+        int l = 0;//left border of 1
+        int r = nums.length - 1;//right border of 2
+        int i = 0;//current index;
+        while(i <= r){//watch "="
+            if (nums[i] == 0){
+                swap(nums, l, i);
+                i++;
+                l++;
+            }else if (nums[i] == 2){
+                swap(nums, r, i);
+                r--;
+            }else{
+                i++;
+            }
+        }
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+}
---
+++ src/org/leecode/Test/Sort_ColorsTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Sort_Colors;
+
+public class Sort_ColorsTest {
+	Sort_Colors p = new Sort_Colors();
+	@Test
+	public void testSortColors() {
+		p.sortColors(new int[]{1,2,0});
+	}
+
+}
