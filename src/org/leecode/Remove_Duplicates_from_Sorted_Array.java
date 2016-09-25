04813e8ad3c8e41e042ff91b51ef3975ce08830e
Xue <Xue@tomy>
2016-09-25 14:42:13 -0500

    update

--- src/org/leecode/Remove_Duplicates_from_Sorted_Array.java
+++ src/org/leecode/Remove_Duplicates_from_Sorted_Array.java
@@ -14,4 +14,18 @@ public class Remove_Duplicates_from_Sorted_Array {
         }
         return j+1;
     }
+	//From Jiu Zhang
+	public int removeDuplicates2(int[] nums) {
+        if (nums.length < 2)
+            return nums.length;
+        int len = 0;
+        //two points
+        for (int i = 1; i< nums.length; i++){
+            if (nums[i] != nums[i - 1]){
+                nums[len+1] = nums[i];
+                len++;
+            }
+        }
+        return len + 1;
+    }
 }
--- src/org/leecode/Swap_Nodes_in_Pairs.java
+++ src/org/leecode/Swap_Nodes_in_Pairs.java
@@ -18,4 +18,39 @@ public class Swap_Nodes_in_Pairs {
         }
         return newHead;
     }
+	//By self
+	public ListNode swapPairs2(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;
+        dummy.next = head;
+        while(head != null && head.next != null){//begin swap next and next.next
+            ListNode temp = head.next.next;
+            head.next.next = pre.next;
+            pre.next = head.next;
+            head.next = temp;
+            pre = pre.next.next;
+            head = pre.next;
+        }
+        return dummy.next;
+    }
+	
+	//From Jiu Zhang
+    public ListNode swapPairs3(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        while(head.next != null && head.next.next != null){
+            ListNode n1 = head.next;
+            ListNode n2 = head.next.next;
+            head.next = n2;
+            n1.next = n2.next;
+            n2.next = n1;
+            head = n1;
+        }
+        return dummy.next;
+    }
 }
