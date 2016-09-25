3585d5940707eaea31203b8143a400356465aeb9
Xue <Xue@tomy>
2016-09-25 16:34:11 -0500

    update

--- src/org/leecode/Move_Zeroes2.java
+++ src/org/leecode/Move_Zeroes2.java
@@ -13,4 +13,16 @@ public class Move_Zeroes2 {
         }
         return nums;
 	}
+	
+	public void moveZeroes2(int[] nums) {
+        int l = 0;
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] != 0){
+                int temp = nums[l];
+                nums[l] = nums[i];
+                nums[i] = temp;
+                l++;
+            }
+        }
+    }
 }
