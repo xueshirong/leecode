20cf59cfd5ad8260825985a9693255c43315631d
Xue <Xue@10.182.57.233>
2016-09-26 00:01:03 -0500

    update

--- src/org/leecode/Search_Insert_Position.java
+++ src/org/leecode/Search_Insert_Position.java
@@ -20,4 +20,17 @@ public class Search_Insert_Position {
         }
         return nums.length;
     }
+	
+	//binary search
+	public int searchInsert3(int[] nums, int target) {
+		int l = 0;
+        int r = nums.length-1;
+        while(l <= r) {
+            int mid = (l + r)/2; 
+            if(target == nums[mid]) return mid;
+            if(target < nums[mid]) r = mid - 1;
+            else l = mid + 1;
+        }
+        return l;
+    }
 }
---
+++ src/org/leecode/Test/Search_Insert_PositionTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Search_Insert_Position;
+
+public class Search_Insert_PositionTest {
+	Search_Insert_Position p = new Search_Insert_Position();
+	@Test
+	public void testSearchInsert3() {
+		p.searchInsert3(new int[]{1,3}, 1);
+	}
+
+}
