c718fdb50b0c4ded2dd65b03585272b22e61e5fb
Xue <Xue@10.182.203.122>
2016-10-01 23:12:38 -0500

    update

--- src/org/leecode/Rotate_Array.java
+++ src/org/leecode/Rotate_Array.java
@@ -18,4 +18,23 @@ public class Rotate_Array {
             nums[i] = temps[i];
         }        
     }
+	//From Jiu Zhang
+	public void rotate2(int[] nums, int k) {
+        if (nums == null || nums.length < 2 || k%nums.length == 0)
+            return;
+        k = k % nums.length;
+        
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+    private void reverse(int[] nums, int l, int r){
+        while (l < r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
 }
