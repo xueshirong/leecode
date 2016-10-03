350f0dd859c20609c006f7719cb9ff8a9ca9c35d
Xue <Xue@10.182.121.17>
2016-10-03 00:04:06 -0500

    update

--- src/org/leecode/Same_Tree.java
+++ src/org/leecode/Same_Tree.java
@@ -20,4 +20,18 @@ public class Same_Tree {
         }
         return true;
     }
+	//From Leetcode
+	public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        boolean left = false;
+        boolean right = false;
+        if (p.val == q.val){//if vals are same, then check left and right node
+            left = isSameTree(p.left, q.left);
+            right = isSameTree(p.right, q.right);
+        }
+        return (left && right);
+    }
 }
