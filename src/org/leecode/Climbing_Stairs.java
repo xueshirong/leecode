21d2168268228e559ec4e2ede8a33f27fda0140d
Xue <Xue@10.182.121.17>
2016-10-02 22:10:10 -0500

    update

--- src/org/leecode/Climbing_Stairs.java
+++ src/org/leecode/Climbing_Stairs.java
@@ -13,4 +13,18 @@ public class Climbing_Stairs {
         }
         return array[n];
     }
+	
+	//a[n] = a[n-1]+a[n-2];
+    public int climbStairs2(int n) {//use DP
+        if (n < 2)
+            return 1;
+        int[] res = new int[n];
+        res[0] = 1;
+        res[1] = 2;
+        for (int i = 2; i < n; i++){
+            res[i] = res[i - 1] + res[i -2];
+        }
+        
+        return res[n - 1];
+    }
 }
