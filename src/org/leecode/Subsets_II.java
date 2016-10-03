6b39c757eed81c6cbca1c04de903b59711bb2185
Xue <Xue@10.182.121.17>
2016-10-02 23:41:24 -0500

    update

--- src/org/leecode/Subsets_II.java
+++ src/org/leecode/Subsets_II.java
@@ -26,4 +26,24 @@ public class Subsets_II {
             list.remove(list.size() -1);
         }
     }
+    //From Jiu Zhang
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0)
+            return res;
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<Integer>();
+        helper2(res, list, nums, 0);
+        return res;
+    }
+    private void helper2(List<List<Integer>> res, List<Integer> list, int[] nums, int pos){
+        res.add(new ArrayList<Integer>(list));
+        for (int i = pos; i < nums.length; i++){
+            if (i != pos && nums[i] == nums[i -1])//if duplicates val, go to next one
+                continue;
+            list.add(nums[i]);
+            helper2(res,list, nums, i+1);
+            list.remove(list.size()-1);
+        }
+    }
 }
