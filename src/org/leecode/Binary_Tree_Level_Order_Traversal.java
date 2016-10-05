31e3e3418e7a2b4255e6534762d4017e1380531e
Xue <Xue@10.182.150.238>
2016-10-04 23:27:47 -0500

    update

--- src/org/leecode/Binary_Tree_Level_Order_Traversal.java
+++ src/org/leecode/Binary_Tree_Level_Order_Traversal.java
@@ -3,6 +3,7 @@ package org.leecode;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Binary_Tree_Level_Order_Traversal {
 	public List<List<Integer>> levelOrder(TreeNode root) {
@@ -20,7 +21,7 @@ public class Binary_Tree_Level_Order_Traversal {
         helper(res, root.left, height+1);
         helper(res, root.right, height+1);
     }
-    
+    //DFS
     public List<List<Integer>> levelOrder2(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         
@@ -35,5 +36,28 @@ public class Binary_Tree_Level_Order_Traversal {
         res.get(height).add(root.val);
         helper2(res, root.left, height + 1);
         helper2(res, root.right, height + 1);
+    }
+    
+  //BFS
+    public List<List<Integer>> levelOrder3(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> list = new ArrayList<Integer>();
+            int height = queue.size();
+            for (int i = 0; i < height; i++){
+                TreeNode head = queue.poll();
+                list.add(head.val);
+                if (head.left != null)
+                    queue.offer(head.left);
+                if (head.right != null)
+                    queue.offer(head.right);
+            }
+            res.add(list);
+        }
+        return res;
     }
 }
