a0cd3a96bf47f00baf7709e4ceaeb8fc6a4bd996
Xue <Xue@tomy>
2016-10-05 01:54:20 -0500

    update

--- src/org/leecode/Balanced_Binary_Tree.java
+++ src/org/leecode/Balanced_Binary_Tree.java
@@ -16,4 +16,22 @@ public class Balanced_Binary_Tree {
         	return -1;
         return Math.max(dl, dr) + 1;
     }
+    
+    //From Discuss
+    boolean res = true;
+    public boolean isBalanced2(TreeNode root) { 
+        maxDepth(root);
+        return res;
+    }
+    
+    int maxDepth(TreeNode node){
+        if (node == null){
+        	return 0;
+        }
+        int dl = maxDepth(node.left);
+        int dr = maxDepth(node.right);
+        if (Math.abs(dl - dr) > 1)
+        	res = false;
+        return Math.max(dl, dr) + 1;
+    }
 }
