6b51893ca72a6cfdd0721a01cbdb5436c0f55f96
Xue <Xue@10.182.150.238>
2016-10-04 22:58:03 -0500

    update

--- src/org/leecode/Binary_Tree_Level_Order_Traversal.java
+++ src/org/leecode/Binary_Tree_Level_Order_Traversal.java
@@ -20,4 +20,20 @@ public class Binary_Tree_Level_Order_Traversal {
         helper(res, root.left, height+1);
         helper(res, root.right, height+1);
     }
+    
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        helper2(res, root, 0);
+        return res;
+    }
+    private void helper2(List<List<Integer>> res, TreeNode root, int height){
+        if (root == null)
+            return;
+        if (res.size() <= height)//key point "<="
+            res.add(new ArrayList<Integer>());
+        res.get(height).add(root.val);
+        helper2(res, root.left, height + 1);
+        helper2(res, root.right, height + 1);
+    }
 }
