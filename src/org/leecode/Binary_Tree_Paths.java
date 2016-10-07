78c9a2b68701c6f967c60d2251ab99fcb7a7dcee
Xue <Xue@tomy>
2016-10-06 23:19:37 -0500

    update

--- src/org/leecode/Binary_Tree_Paths.java
+++ src/org/leecode/Binary_Tree_Paths.java
@@ -11,7 +11,7 @@ public class Binary_Tree_Paths {
 	        helper(res, "", root);
 	        return res;
 	    }
-	    private void helper(List<String> res,String path, TreeNode root){
+	 private void helper(List<String> res,String path, TreeNode root){
 	        if (root.left == null && root.right == null)
 	            res.add(path + root.val);
 	        if (root.right != null)
@@ -19,4 +19,24 @@ public class Binary_Tree_Paths {
 	        if (root.left != null)
 	            helper(res, path + root.val + "->", root.left);
 	    }
+	 //From Jiu Zhang
+	 public List<String> binaryTreePaths2(TreeNode root) {
+	        List<String> res = new ArrayList<String>();
+	        if (root == null)
+	            return res;
+	        helper2(res, "", root);
+	        return res;
+	    }
+	    private void helper2(List<String> res, String s, TreeNode root){
+	        s += root.val;
+	        if (root.left == null && root.right == null){
+	            res.add(s);
+	            return;
+	        }
+	        if (root.left != null)
+	            helper2(res, s + "->", root.left);
+	        if (root.right != null)
+	            helper2(res, s + "->", root.right);
+	    }
+	    
 }
