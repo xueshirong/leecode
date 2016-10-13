8879d6b514460ee14ecc3e0f4a9d558c3b728b76
Xue <Xue@10.182.230.217>
2016-10-13 18:13:15 -0500

    update

--- src/org/leecode/Best_Time_to_Buy_and_Sell_Stock.java
+++ src/org/leecode/Best_Time_to_Buy_and_Sell_Stock.java
@@ -15,4 +15,20 @@ public class Best_Time_to_Buy_and_Sell_Stock {
         }
         return profit;
     }
+	//From Jiu Zhang
+	public int maxProfit2(int[] prices) {
+        int profit = 0;
+        if (prices == null || prices.length == 0)
+            return profit;
+        int min = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < prices.length; i++){
+            if (prices[i] < min)
+                min = prices[i];
+            if (prices[i] - min > profit)
+                profit = prices[i] - min;
+        }
+        
+        return profit;
+    }
 }
--- src/org/leecode/Best_Time_to_Buy_and_Sell_Stock_II.java
+++ src/org/leecode/Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -9,4 +9,16 @@ public class Best_Time_to_Buy_and_Sell_Stock_II {
         }
         return profit;
     }
+	//From Jiu Zhang
+	public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0)
+            return 0;
+        int max = 0;
+        for (int i = 0; i < prices.length - 1; i++){
+            int dif = prices[i+1] -  prices[i];  
+            if (dif > 0)
+                max += dif;
+        }
+        return max;
+    }
 }
