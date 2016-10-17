6cd832ed25a874f9e9f8b013cfc2b5c6e8f95836
Xue <Xue@10.182.137.79>
2016-10-16 20:43:03 -0500

    new

---
+++ src/org/leecode/Find_the_Duplicate_Number.java
@@ -0,0 +1,27 @@
+package org.leecode;
+
+public class Find_the_Duplicate_Number {
+	//From Internet
+	public int findDuplicate(int[] nums) {
+        int min = 0, max = nums.length - 1;
+        while(min <= max){
+            // 找到中间那个数
+            int mid = min + (max - min) / 2;
+            int cnt = 0;
+            // 计算总数组中有多少个数小于等于中间数
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i] <= mid){
+                    cnt++;
+                }
+            }
+            // 如果小于等于中间数的数量大于中间数，说明前半部分必有重复
+            if(cnt > mid){
+                max = mid - 1;
+            // 否则后半部分必有重复
+            } else {
+                min = mid + 1;
+            }
+        }
+        return min;
+    }
+}
