c398a6a25b235023190e94bc9da02eba08b29a3b
Xue <Xue@10.182.137.79>
2016-10-16 19:19:15 -0500

    update

--- src/org/leecode/Linked_List_Cycle.java
+++ src/org/leecode/Linked_List_Cycle.java
@@ -17,4 +17,18 @@ public class Linked_List_Cycle {
         }
         return false;
     }
+	
+	public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
 }
