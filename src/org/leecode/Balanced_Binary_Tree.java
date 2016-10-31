76a99acd0623ee03556a50477b297cb9cc7d4df1
Xue <Xue@tomy>
2016-10-30 23:59:55 -0500

    update

--- src/org/leecode/Balanced_Binary_Tree.java
+++ src/org/leecode/Balanced_Binary_Tree.java
@@ -34,4 +34,63 @@ public class Balanced_Binary_Tree {
         	res = false;
         return Math.max(dl, dr) + 1;
     }
+    
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    //From Jiu Zhang
+    public boolean isBalanced3(TreeNode root) {
+        // write your code here
+        if (root == null)
+            return true;
+        
+        if (helper(root) == -1)
+            return false;
+        return true;
+    }
+    private int helper(TreeNode root){
+        if (root == null)
+            return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        
+        if (left == -1 || right == -1 || Math.abs(left -right) > 1)
+            return -1;
+        return (Math.max(left, right) + 1);
+    }
+    
+    /**
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    //From Jiu Zhang
+    class subType{
+        public boolean isBlanced;
+        public int depth;
+        public subType(boolean isBlanced, int depth){
+            this.isBlanced = isBlanced;
+            this.depth = depth;
+        }
+    }
+    public boolean isBalanced5(TreeNode root) {
+        // write your code here
+        subType res = helper5(root);
+        return res.isBlanced;
+    }
+    private subType helper5(TreeNode root){
+        if (root == null)
+            return new subType(true, 0);
+            
+        subType left = helper5(root.left);
+        subType right = helper5(root.right);
+        
+        if (!left.isBlanced || !right.isBlanced)
+            return new subType(false, -1);
+        
+        if (Math.abs(left.depth - right.depth) > 1)
+            return new subType(false, -1);
+            
+        return new subType(true, Math.max(left.depth, right.depth) + 1);
+    }
 }
