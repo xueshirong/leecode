e6e53572328ac725d56e4cf4853e046219b8f217
Xue <Xue@tomy>
2016-10-31 23:58:18 -0500

    update

--- src/org/leecode/Binary_Tree_Preorder_Traversal.java
+++ src/org/leecode/Binary_Tree_Preorder_Traversal.java
@@ -1,6 +1,7 @@
 package org.leecode;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Binary_Tree_Preorder_Traversal {
 	//Given a binary tree, return the preorder traversal of its nodes' values.
@@ -21,5 +22,23 @@ public class Binary_Tree_Preorder_Traversal {
         res.add(root.val);
         helper(root.left, res);
         helper(root.right, res);
+    }
+    //From Jiu Zhang
+    public ArrayList<Integer> preorderTraversal2(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        
+        if (root == null)
+            return res;
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+        }
+        return res;
     }
 }
