300e3cff24741caace4e9e29a05776c8f2548055
Xue <Xue@tomy>
2016-11-02 22:22:49 -0500

    update

--- src/org/leecode/Binary_Tree_Level_Order_Traversal.java
+++ src/org/leecode/Binary_Tree_Level_Order_Traversal.java
@@ -60,4 +60,58 @@ public class Binary_Tree_Level_Order_Traversal {
         }
         return res;
     }
+    //From Jiu Zhang BFS
+    public ArrayList<ArrayList<Integer>> levelOrder4(TreeNode root) {
+    	// write your code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();//size of queue is changing, so must get it beforehand
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+    //From Jiu Zhang 2 queue
+    public ArrayList<ArrayList<Integer>> levelOrder5(TreeNode root) {
+        // write your code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if (root == null)
+            return res;
+        //q1 is used to store current level nodes
+        ArrayList<TreeNode> q1 = new ArrayList<TreeNode>();
+        //q1 is used to store next level nodes
+        ArrayList<TreeNode> q2 = new ArrayList<TreeNode>();
+        q1.add(root);
+        while (!q1.isEmpty()){
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            q2.clear();
+            for (int i = 0; i < q1.size(); i++){
+                TreeNode node = q1.get(i);
+                level.add(node.val);
+                if (node.left != null)
+                    q2.add(node.left);
+                if (node.right != null)
+                    q2.add(node.right);
+            }
+            //swap q1 q2
+            ArrayList<TreeNode> temp = q1;
+            q1 = q2;
+            q2 = temp;
+            
+            res.add(level);
+        }
+        return res;
+    }
 }
