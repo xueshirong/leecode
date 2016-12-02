a2a75cf7c4e8cacac58d316cc9b81c7d0ee6f43f
Xue <Xue@tomy>
2016-12-02 00:12:45 -0600

    update

--- src/org/leecode/Merge_Sorted_Array.java
+++ src/org/leecode/Merge_Sorted_Array.java
@@ -49,4 +49,36 @@ public class Merge_Sorted_Array {
             index--;
         }
     }
+	//-------------------
+	//Thought: from end of A and B, compare element, put larger one to the end of A.
+	//Given two sorted integer arrays A and B, merge B into A as one sorted array.
+	// Notice
+	//You may assume that A has enough space (size that is greater or equal to m + n) to 
+	//hold additional elements from B. The number of elements initialized in A and B are m and n 
+	//respectively.
+    /**
+     * @param A: sorted integer array A which has m elements, 
+     *           but size of A is m+n
+     * @param B: sorted integer array B which has n elements
+     * @return: void
+     */
+    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
+        //corner case
+        if (B == null || n == 0)
+            return;
+        while (m > 0 && n > 0){
+            if (A[m-1] > B[n-1]){
+                A[m+n-1] = A[m-1];
+                m--;
+            }else{
+                A[m+n-1] = B[n-1];
+                n--;
+            }
+        }
+        if (n > 0){
+            for(int i = 0; i < n; i++){
+                A[i] = B[i];
+            }
+        }
+    }
 }
