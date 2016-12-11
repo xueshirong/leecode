376480cd30fa7b668d3d3d8c8d46c428a71c7900
Xue <Xue@tomy>
2016-12-10 23:52:03 -0600

    update

--- src/org/leecode/Implement_Queue_using_Stacks.java
+++ src/org/leecode/Implement_Queue_using_Stacks.java
@@ -38,3 +38,42 @@ public class Implement_Queue_using_Stacks {
         return in.empty() && out.empty();
     }
 }
+//----------solution 2
+class MyQueue {
+    Stack<Integer> in = new Stack<Integer>();
+    Stack<Integer> out = new Stack<Integer>();
+    
+    // Push element x to the back of queue.
+    public void push(int x) {
+        in.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        if (out.isEmpty()){
+            cpoyIn2Out();
+        }
+        out.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        if (out.isEmpty()){
+            cpoyIn2Out();
+        }
+        return out.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        if (in.isEmpty() && out.isEmpty())
+            return true;
+        return false;
+    }
+    
+    private void cpoyIn2Out(){
+        while (!in.isEmpty()){
+            out.push(in.pop());
+        }
+    }
+}
