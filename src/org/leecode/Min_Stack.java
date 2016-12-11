3ff35a705b1fdc224ee45f0718c2875cde04f5ee
Xue <Xue@tomy>
2016-12-10 23:51:39 -0600

    update

--- src/org/leecode/Min_Stack.java
+++ src/org/leecode/Min_Stack.java
@@ -1,5 +1,8 @@
 package org.leecode;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Min_Stack {
@@ -39,5 +42,70 @@ public class Min_Stack {
         if (!min.empty())
             return min.peek();
         return 0;
+    }
+}
+
+class MinStack2 {
+    //to save min vals
+    PriorityQueue<Integer> q;
+    Stack<Integer> stack;
+    Queue<Integer> qq;
+    
+    public MinStack2() {
+        // do initialize if necessary
+        q = new PriorityQueue<Integer>();
+        stack = new Stack<Integer>();
+        qq = new LinkedList<Integer>();
+    }
+
+    public void push(int number) {
+    	qq.add(number);
+    	qq.offer(number);
+    	qq.poll();
+    	qq.peek();
+    	qq.isEmpty();
+    	
+    	q.add(number);
+    	q.offer(number);
+    	q.poll();
+    	q.peek();
+    	q.isEmpty();
+    	
+    	stack.add(number);
+    	stack.pop();
+    	stack.peek();
+    	stack.isEmpty();
+    }
+}
+//---solution 3
+class MinStack3 {
+	Stack<Integer> res;
+    Stack<Integer> minS;
+    /** initialize your data structure here. */
+    public MinStack3() {
+        res = new Stack<Integer>();
+        minS = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        if (!minS.isEmpty())
+            minS.push(Math.min(x, minS.peek()));
+        else
+            minS.push(x);
+            
+        res.push(x);
+    }
+    
+    public void pop() {
+        res.pop();
+        minS.pop();
+    }
+    
+    public int top() {
+        return res.peek();
+    }
+    
+    public int getMin() {
+        return minS.peek();
     }
 }
