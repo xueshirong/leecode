cc527fa930cb89a683f899017455fe41f69900a8
Xue <Xue@tomy>
2016-12-22 22:19:37 -0600

    new

---
+++ src/org/leecode/Number_of_Ways_to_Represent_N_Cents.java
@@ -0,0 +1,20 @@
+package org.leecode;
+
+public class Number_of_Ways_to_Represent_N_Cents {
+	/**
+     * @param n an integer
+     * @return an integer
+     */
+    public int waysNCents(int n) {
+        int[] f = new int[n+1];
+        f[0] = 1;
+        int[] cents = new int[]{1, 5, 10, 25};
+        for (int i = 0; i < 4; i++) 
+            for (int j = 1; j <= n; j++) {
+                if (j >= cents[i]) {
+                    f[j] += f[j-cents[i]];
+                }
+            }
+        return f[n];
+    }
+}
---
+++ src/org/leecode/Test/Number_of_Ways_to_Represent_N_CentsTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Number_of_Ways_to_Represent_N_Cents;
+
+public class Number_of_Ways_to_Represent_N_CentsTest {
+	Number_of_Ways_to_Represent_N_Cents p = new Number_of_Ways_to_Represent_N_Cents();
+	@Test
+	public void testWaysNCents() {
+		p.waysNCents(100);
+	}
+
+}
