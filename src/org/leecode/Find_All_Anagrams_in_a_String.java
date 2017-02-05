782cd27f837908db884a7a887b75d0a8b5f8ecfd
Xue <Xue@Diabolo>
2017-02-04 23:36:45 -0600

    new

--- src/org/leecode/Find_All_Anagrams_in_a_String.java
+++ src/org/leecode/Find_All_Anagrams_in_a_String.java
@@ -15,19 +15,30 @@ public class Find_All_Anagrams_in_a_String {
 	    //two points, initialize count to p's length
 	    int left = 0, right = 0, count = p.length();
 	    while (right < s.length()) {
-	        //move right everytime, if the character exists in p's hash, decrease the count
+	        //move right every time, if the character exists in p's hash, decrease the count
 	        //current hash value >= 1 means the character is existing in p
-	        if (hash[s.charAt(right++)]-- >= 1) count--; 
+	    	if (hash[s.charAt(right)] >= 1) {
+	            count--;
+	        }
+	        hash[s.charAt(right)]--;
+	        right++;
 	        
 	        //when the count is down to 0, means we found the right anagram
 	        //then add window's left to result list
-	        if (count == 0) list.add(left);
-	    
+	        if (count == 0) {
+                list.add(left);
+            }
 	        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
 	        //++ to reset the hash because we kicked out the left
 	        //only increase the count if the character is in p
 	        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
-	        if (right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+	        if (right - left == p.length() ) {
+	            if (hash[s.charAt(left)] >= 0) {
+	                count++;
+	            }
+	            hash[s.charAt(left)]++;
+	            left++;
+	        }       
 	    }
 	    return list;
 	}
