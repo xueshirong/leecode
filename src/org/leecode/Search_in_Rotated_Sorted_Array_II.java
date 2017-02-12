18667d65001a68d821f8c5f8349b64cb97207c7d
Xue <Xue@Diabolo>
2017-02-12 12:13:47 -0600

    update

--- src/org/leecode/Search_in_Rotated_Sorted_Array_II.java
+++ src/org/leecode/Search_in_Rotated_Sorted_Array_II.java
@@ -42,4 +42,41 @@ public class Search_in_Rotated_Sorted_Array_II {
             return true;
         return false;
     }
+    
+    // 这个问题在面试中不会让实现完整程序
+    // 只需要举出能够最坏情况的数据是 [1,1,1,1... 1] 里有一个0即可。
+    // 在这种情况下是无法使用二分法的，复杂度是O(n)
+    // 因此写个for循环最坏也是O(n)，那就写个for循环就好了
+    //  如果你觉得，不是每个情况都是最坏情况，你想用二分法解决不是最坏情况的情况，那你就写一个二分吧。
+    //  反正面试考的不是你在这个题上会不会用二分法。这个题的考点是你想不想得到最坏情况。
+    public boolean search2(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return false;
+            
+        int s = 0, e = nums.length - 1;
+        while(s + 1 < e){
+            int mid = s + (e - s)/2;
+            if (nums[mid] == target)
+                return true;
+                
+            if (nums[s] < nums[mid]){
+                if (nums[s] <= target && target <= nums[mid])
+                    e = mid;
+                else
+                    s = mid;
+            }
+            else if (nums[s] > nums[mid]){
+                if (nums[mid] <= target && target <= nums[e])
+                    s = mid;
+                else
+                    e = mid;
+            }
+            else//skip duplicate one, A[start] == A[mid]
+                s++;
+        }
+        
+        if (nums[s] == target || nums[e] == target)
+            return true;
+        return false;
+    }
 }
