0346594e1ab0595f266497e84eaaabdb27235ded
Xue <Xue@Diabolo>
2017-02-12 02:35:23 -0600

    update

--- src/org/leecode/Sqrt_x.java
+++ src/org/leecode/Sqrt_x.java
@@ -14,4 +14,26 @@ public class Sqrt_x {
         
         return left;
     }
+	
+	//binary search
+    public int mySqrt(int x) {
+        if (x <= 1)
+            return x;
+        int s = 1, e = x;
+        while (s + 1 < e){
+            int mid = s + (e - s)/2;
+            
+            if (x/mid == mid){//use x/mid in order to avoid overflow
+                return mid;
+            }else if(x/mid > mid){
+                s = mid;
+            }else
+                e = mid;
+        }
+        if (s <= x/s)
+            return s;
+        if (e <= x/e)
+            return e;
+        return 0;
+    }
 }
