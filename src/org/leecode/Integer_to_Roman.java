1d4ac8bf2624f9bfdebb8bfd0ce753e1d37a7ebf
Xue <Xue@Diabolo>
2017-02-11 21:05:52 -0600

    update

--- src/org/leecode/Integer_to_Roman.java
+++ src/org/leecode/Integer_to_Roman.java
@@ -17,4 +17,24 @@ public class Integer_to_Roman {
 	        }
 	        return res;
 	 }
+	 
+	 //solution 2
+	//1. list all possible numbers for roman (1,4,5,9...) and (I, IV, V IX...)
+	    //2. calculate times between num and symbol[i], and append them
+	    public String intToRoman1(int num) {
+	        //corner case
+	        if (num < 1 || num > 3999)
+	            return "";
+	        StringBuilder sb = new StringBuilder();
+	        int[] nums = new int[]{1,4,5,9,10,40,50,90,100,400,500,900,1000};
+	        String[] symbols = new String[]{"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+	        for (int i = nums.length - 1; i > -1; i--){
+	            int times = num / nums[i];
+	            for (int j = 0; j < times; j++){
+	                sb.append(symbols[i]);
+	            }
+	            num = num - times * nums[i];
+	        }
+	        return sb.toString();
+	    }
 }
---
+++ src/org/leecode/Test/Integer_to_RomanTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Integer_to_Roman;
+
+public class Integer_to_RomanTest {
+	Integer_to_Roman p = new Integer_to_Roman();
+	@Test
+	public void testIntToRoman1() {
+		p.intToRoman1(3299);
+	}
+
+}
