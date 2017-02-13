1641e4cac006c4a8f1c9795438c3c32ca9be062a
Xue <Xue@Diabolo>
2017-02-13 14:50:05 -0600

    update

--- src/org/leecode/Isomorphic_Strings.java
+++ src/org/leecode/Isomorphic_Strings.java
@@ -3,18 +3,17 @@ package org.leecode;
 import java.util.Hashtable;
 
 public class Isomorphic_Strings {
+	//use map to save charater's mapping relation. e:a, g:d
 	public boolean isIsomorphic(String s, String t) {
 		Hashtable<Character, Character> table = new Hashtable<Character, Character>();
         for (int i = 0; i < s.length(); i++){
             char cS = s.charAt(i);
             char cT = t.charAt(i);
             if (table.containsKey(cS)){
-                if (table.get(cS).equals(cT)){
-                    continue;
-                }else{
+                if (!table.get(cS).equals(cT)){
                     return false;
                 }
-            }else if (table.containsValue(cT)){
+            }else if (table.containsValue(cT)){//key point to check if t is in the value
                 return false;
             }else{
                 table.put(cS,cT);
