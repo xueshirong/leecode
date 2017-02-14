38c7196ecdbe7185d8a4f4e970198dbb9c6dfd17
Xue <Xue@Diabolo>
2017-02-13 22:36:13 -0600

    update

--- src/org/leecode/TopK_Frequent.java
+++ src/org/leecode/TopK_Frequent.java
@@ -1,5 +1,7 @@
 package org.leecode;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,7 +9,39 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 public class TopK_Frequent {
+	//from jiu zhang
 	public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> hashmap = new HashMap<Integer, Integer>();
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<Map.Entry<Integer, Integer>>(
+            new Comparator<Map.Entry<Integer, Integer>>() {
+                public int compare(Map.Entry<Integer, Integer> e1, Map.Entry<Integer, Integer> e2) {
+                    return e1.getValue() - e2.getValue();
+                }
+            });
+        for (int i = 0; i < nums.length; i++) {
+            if (!hashmap.containsKey(nums[i])) {
+                hashmap.put(nums[i], 1);
+            } else {
+                hashmap.put(nums[i], hashmap.get(nums[i]) + 1);
+            }
+        }
+        
+        for (Map.Entry<Integer, Integer> entry : hashmap.entrySet()) {
+            if (queue.size() < k) {
+                queue.offer(entry);
+            } else if (queue.peek().getValue() < entry.getValue()) {
+                queue.poll();
+                queue.offer(entry);
+            }
+        }
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        for (Map.Entry<Integer, Integer> entry : queue)
+            ans.add(entry.getKey());
+        return ans;
+    }
+	
+	public List<Integer> topKFrequent1(int[] nums, int k) {
 		Map<Integer, Integer> counterMap = new HashMap<>();
 		for (int num : nums) {
 			int count = counterMap.getOrDefault(num, 0);
