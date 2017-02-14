0d95cc8f3ed22c82c508d9c2228d8f7d727d8a18
Xue <Xue@Diabolo>
2017-02-14 00:09:01 -0600

    new

---
+++ src/org/leecode/DS_diff_of_linkedlist_and_arraylist.java
@@ -0,0 +1,15 @@
+package org.leecode;
+
+public class DS_diff_of_linkedlist_and_arraylist {
+	/*
+	 * I enjoy the classic "what's the difference between a LinkedList and an ArrayList 
+	 * (or between a linked list and an array/vector) and why would you choose one or the other?"
+	 * 
+	 * The kind of answer I hope for is one that includes discussion of:
+	 * insertion performance
+	 * iteration performance
+	 * memory allocation/reallocation impact
+	 * impact of removing elements from the beginning/middle/end
+	 * how knowing (or not knowing) the maximum size of the list can affect the decision
+	 */
+}
