ab774c6c26da6e53c4c3fd808097bd0ddc1f3e49
Xue <Xue@Diabolo>
2017-02-13 22:23:58 -0600

    update

--- src/org/leecode/Nested_List_Weight_Sum.java
+++ src/org/leecode/Nested_List_Weight_Sum.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.Queue;
 
 public class Nested_List_Weight_Sum {
+	//Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+	//Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+	//Example 1:
+	//Given the list [[1,1],2,[1,1]], return 10. (four 1's at depth 2, one 2 at depth 1)
+	
 	//递归 DFS
 	public int depthSum(List<NestedInteger> nestedList) {
         return helper(nestedList, 1);
