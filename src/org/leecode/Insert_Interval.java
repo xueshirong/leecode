596f70ed7c5a1edd2110b63db6c54d570b45e00e
Xue <Xue@Diabolo>
2017-02-14 03:51:13 -0600

    new

---
+++ src/org/leecode/Insert_Interval.java
@@ -0,0 +1,37 @@
+package org.leecode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.leecode.Merge_Intervals.Interval;
+
+public class Insert_Interval {
+	/**
+	 * Definition for an interval.
+	 * public class Interval {
+	 *     int start;
+	 *     int end;
+	 *     Interval() { start = 0; end = 0; }
+	 *     Interval(int s, int e) { start = s; end = e; }
+	 * }
+	 */
+	
+	public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> result = new ArrayList<Interval>();
+        for (Interval i : intervals) {
+            if (newInterval == null || i.end < newInterval.start)
+                result.add(i);
+            else if (i.start > newInterval.end) {
+                result.add(newInterval);
+                result.add(i);
+                newInterval = null;
+            } else {
+                newInterval.start = Math.min(newInterval.start, i.start);
+                newInterval.end = Math.max(newInterval.end, i.end);
+            }
+        }
+        if (newInterval != null)
+            result.add(newInterval);
+        return result;
+    }
+}
