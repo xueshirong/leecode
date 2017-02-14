0dbec4312ee2805086e7c30a070413bab0a79958
Xue <Xue@Diabolo>
2017-02-14 01:09:24 -0600

    update

--- src/org/leecode/Two_Sum_III_Data_structure_design.java
+++ src/org/leecode/Two_Sum_III_Data_structure_design.java
@@ -1,7 +1,10 @@
 package org.leecode;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class Two_Sum_III_Data_structure_design {
 	/*Design and implement a TwoSum class. It should support the following operations: add and find.
@@ -17,7 +20,7 @@ public class Two_Sum_III_Data_structure_design {
 	关键点在于对重复数字的处理，用hashmap存储值，然后判断value-num == num的情况下，count是否>=2.
 	*/
 }
-
+//solution 1: find is less and add is more
 class TwoSum {
     private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     // Add the number to an internal data structure.
@@ -42,4 +45,31 @@ class TwoSum {
 		}
         return false;
     }
+}
+//more find and less add
+class TwoSum2 {
+    Set<Integer> sum;
+    Set<Integer> num;
+    
+    TwoSum2(){
+        sum = new HashSet<Integer>();
+        num = new HashSet<Integer>();
+    }
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	    if(num.contains(number)){
+	        sum.add(number * 2);
+	    }else{
+	        Iterator<Integer> iter = num.iterator();
+	        while(iter.hasNext()){
+	            sum.add(iter.next() + number);
+	        }
+	        num.add(number);
+	    }
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    return sum.contains(value);
+	}
 }
