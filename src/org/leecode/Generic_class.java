4ef141747e91f3e1b1887f8f9612ad481ceced9d
Xue <Xue@Diabolo>
2017-02-13 23:26:43 -0600

    new

---
+++ src/org/leecode/Generic_class.java
@@ -0,0 +1,22 @@
+package org.leecode;
+
+public class Generic_class {
+	/*
+	 * https://www.tutorialspoint.com/java/java_generics.htm
+	 * It would be nice if we could write a single sort method that could sort the elements in an 
+	 * Integer array, a String array, or an array of any type that supports ordering.
+	 * 
+	 * Java Generic methods and generic classes enable programmers to specify, with a single method declaration, 
+	 * a set of related methods, or with a single class declaration, a set of related types, respectively.
+	 * 
+	 * Generics also provide compile-time type safety that allows programmers to catch invalid types at 
+	 * compile time.
+	 * Using Java Generic concept, we might write a generic method for sorting an array of objects, 
+	 * then invoke the generic method with Integer arrays, Double arrays, String arrays and so on, 
+	 * to sort the array elements.
+	 * 
+	 * <T> specifically stands for generic type. According to Java Docs - A generic type is a generic class or interface that is 
+	 * parameterized over types. 
+	 * Let me start with an example: Consider a Box type that has two methods which is used to set and get objects.
+	 */
+}
