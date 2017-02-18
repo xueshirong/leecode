3409165385f5c0a12975d76326778a067439ca6f
Xue <Xue@129.107.211.38>
2017-02-18 16:36:29 -0600

    update

--- src/org/leecode/House_Robber.java
+++ src/org/leecode/House_Robber.java
@@ -1,6 +1,7 @@
 package org.leecode;
 
 public class House_Robber {
+	//Time O(n), Space O(1)
 	public int rob(int[] nums) {
         if (nums.length == 0){
             return 0;
@@ -16,5 +17,22 @@ public class House_Robber {
             lastMax = temp;
         }
         return curMax;
+    }
+	//from jiu zhang dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+	//Time O(n), Space O(n)
+	public long houseRobber1(int[] A) {
+        // write your code here
+        int n = A.length;
+        if(n == 0)
+            return 0;
+        long []res = new long[n+1];
+        
+        
+        res[0] = 0;
+        res[1] = A[0];
+        for(int i = 2; i <= n; i++) {
+            res[i] = Math.max(res[i-1], res[i-2] + A[i-1]);
+        }
+        return res[n];
     }
 }
---
+++ src/org/leecode/Test/House_RobberTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.House_Robber;
+
+public class House_RobberTest {
+	House_Robber p = new House_Robber();
+	@Test
+	public void testRob() {
+		p.rob(new int[]{3,1,2,5});
+	}
+
+}
