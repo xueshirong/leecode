adcb054bd579810d977c69fff79f47648b0da025
Xue <Xue@Diabolo>
2017-02-18 00:43:12 -0600

    update

--- src/org/leecode/Binary_Search_Tree_Iterator.java
+++ src/org/leecode/Binary_Search_Tree_Iterator.java
@@ -3,7 +3,6 @@ import java.util.Stack;
 
 public class Binary_Search_Tree_Iterator {
 	public class BSTIterator {
-
 		TreeNode cur = null;
 		Stack<TreeNode> stack = new Stack<TreeNode>();
 	    public BSTIterator(TreeNode root) {
@@ -16,7 +15,9 @@ public class Binary_Search_Tree_Iterator {
 	        	return true;
 	    	return false;
 	    }
-
+	    //非递归的中序遍历，获取下一个。
+	    //维护一个栈，从根节点开始，每次迭代地将根节点的左孩子压入栈，直到左孩子为空为止。
+	    //调用next()方法时，弹出栈顶，如果被弹出的元素拥有右孩子，则以右孩子为根，将其左孩子迭代压栈。
 	    /** @return the next smallest number */
 	    public TreeNode  next() {
 	    	while (cur != null) {
