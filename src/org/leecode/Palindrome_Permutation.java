5e070937c6a18f4b700c041b9c8c43b39f0760af
Xue <Xue@Diabolo>
2017-03-04 10:55:51 -0600

    update

--- src/org/leecode/Palindrome_Permutation.java
+++ src/org/leecode/Palindrome_Permutation.java
@@ -1,9 +1,11 @@
 package org.leecode;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Palindrome_Permutation {
-	public boolean canPermutePalindrome(String s) {
+	
+	public boolean canPermutePalindrome1(String s) {
 		HashSet<Character> hs = new HashSet<Character>();
 		for(int i = 0; i < s.length(); i++){
 			if (hs.contains(s.charAt(i))){
@@ -17,4 +19,17 @@ public class Palindrome_Permutation {
 			return true;		
 		return false;
 	}
+	//set
+	public boolean canPermutePalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return true;
+        Set<Character> set = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            set.add(c);
+        }
+        if (set.size() < 2)
+            return true;
+        return false;
+    }
 }
