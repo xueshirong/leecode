46186dfc92566680b7383fe758458cf235a42c76
Xue <Xue@Diabolo>
2017-03-04 17:40:50 -0600

    new

--- src/org/leecode/Detect_Capital.java
+++ src/org/leecode/Detect_Capital.java
@@ -1,10 +1,12 @@
 package org.leecode;
 
 public class Detect_Capital {
-	public boolean detectCapitalUse1(String word) {
+	//用变量upper记录大写字母的个数。
+    //如果upper== 0,说明全是小写；upper=word长度，说明全是大写；upper=1，只需要判断是否第一个字母为大写
+    public boolean detectCapitalUse(String word) {
         boolean isFirstCap = false;
         int upper = 0;
-        int lower = 0;
+        
         for (int i = 0; i < word.length(); i++){
             char c = word.charAt(i);
             if (i == 0 && Character.isUpperCase(c)){
@@ -14,16 +16,14 @@ public class Detect_Capital {
             }
             if (Character.isUpperCase(c)){
                 upper++;
-            }else{
-                lower++;
             }
         }
-        if(upper == word.length() || lower == word.length() || (isFirstCap && lower == word.length() - 1))
+        if(upper == word.length() || upper == 0 || (isFirstCap && upper == 1))
             return true;
         return false;
     }
     //solution 2
-    public boolean detectCapitalUse(String word) {
+    public boolean detectCapitalUse1(String word) {
         if (word.toUpperCase().equals(word))//判断全部是大写
             return true;
         if (word.toLowerCase().equals(word))//判断全部是小写
