0cb8171c2d9d9cbd9890a3ecbd1612e8e32b3bf3
Xue <Xue@129.107.211.36>
2017-03-18 22:46:49 -0500

    update

--- src/org/leecode/Combination_Sum_IV.java
+++ src/org/leecode/Combination_Sum_IV.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Combination_Sum_IV {
+public class Combination_Sum_IV { 
 	//dfs recursive will time out
     public int combinationSum41(int[] nums, int target) {
         if (nums == null || nums.length == 0)
