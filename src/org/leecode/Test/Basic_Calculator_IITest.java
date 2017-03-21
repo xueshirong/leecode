846a068797200835d539202d90a7672d3d9dea81
Xue <Xue@Diabolo>
2017-03-21 13:50:27 -0500

    new

---
+++ src/org/leecode/Test/Basic_Calculator_IITest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Basic_Calculator_II;
+
+public class Basic_Calculator_IITest {
+	Basic_Calculator_II p  = new Basic_Calculator_II();
+	@Test
+	public void testCalculate() {
+		p.calculate("3*2+4");
+	}
+
+}
