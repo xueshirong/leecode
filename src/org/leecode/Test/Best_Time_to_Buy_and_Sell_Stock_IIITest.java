5a6b48996fe4783ab8302b1939ae1b0177b4240e
xueshr <xueshirong@gmail.com>
2017-04-02 17:05:36 -0500

    update

--- src/org/leecode/Test/Best_Time_to_Buy_and_Sell_Stock_IIITest.java
+++ src/org/leecode/Test/Best_Time_to_Buy_and_Sell_Stock_IIITest.java
@@ -7,7 +7,7 @@ public class Best_Time_to_Buy_and_Sell_Stock_IIITest {
 	Best_Time_to_Buy_and_Sell_Stock_III p = new Best_Time_to_Buy_and_Sell_Stock_III();
 	@Test
 	public void testMaxProfit() {
-		p.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+		p.maxProfit(new int[]{1,4,5,7,6,3,2,9});
 	}
 
 }
