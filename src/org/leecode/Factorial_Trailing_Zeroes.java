29bb85c991cbd275b8b654e8f1f8494b144ad90c
Xue <Xue@Diabolo>
2017-04-25 22:52:46 -0500

    upate

--- src/org/leecode/Factorial_Trailing_Zeroes.java
+++ src/org/leecode/Factorial_Trailing_Zeroes.java
@@ -1,13 +1,24 @@
 package org.leecode;
 
 public class Factorial_Trailing_Zeroes {
-	public int trailingZeroes(int n) {
-		 int num = 0;
-	        while(n/5 >0){
-	            n = n/5;
-	            num += n;
-	        }
-	        
-	        return num;
+	//技巧在于，每5个数会产生一个0
+    //iterator
+    public int trailingZeroes1(int n) {
+        int num = 0;
+        while(n/5 >0){
+            // 阶乘中有多少5，结果就有多少个0
+            n = n/5;
+            num += n;
+        }
+        
+        return num;
+    }
+    
+    //recursion
+    public int trailingZeroes(int n) {
+        if(n == 0)
+            return 0;
+        else
+            return n/5 + trailingZeroes(n/5);
     }
 }
