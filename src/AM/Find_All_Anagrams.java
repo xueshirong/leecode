ccfe7fbd43a076d361016b5b349ffcb188f22c20
xueshr <xueshirong@gmail.com>
2017-05-31 17:57:58 -0500

    update

--- src/AM/Find_All_Anagrams.java
+++ src/AM/Find_All_Anagrams.java
@@ -8,38 +8,43 @@ public class Find_All_Anagrams {
 		System.out.print(res);
 	}
 	public static List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
-    
-        int[] hash = new int[256]; 
-    
-        for (char c : p.toCharArray()) {
-            hash[c]++;
+		List<Integer> ans = new ArrayList <Integer>();
+		if (s == null || s.length() == 0 || p == null || p.length() == 0) 
+			return ans;
+		//record each character in p to array
+        int[] arr = new int[30];
+
+        int plength = p.length();
+        int slength = s.length();
+        
+        for(char c : p.toCharArray()){
+        	arr[c - 'a'] ++;
         }
-    
-        int left = 0, right = 0, count = p.length();
-    
-        while (right < s.length()) {
-    
-            if (hash[s.charAt(right)] >= 1) {
-                count--;
+        //two points
+        int start = 0;
+        int end = 0;
+        int matched = 0;
+        while(end < slength){
+        	//if char is in p, then matched increase 1
+            if(arr[s.charAt(end) - 'a'] >= 1){
+                matched ++;
             }
-            hash[s.charAt(right)]--;
-            right++;
-    
-            if (count == 0) {
-                list.add(left);
+            
+            arr[s.charAt(end) - 'a'] --;
+            end ++;//move right every time
+            
+            if(matched == plength) {//get the correct anagram
+                ans.add(start);//add to result
             }
-    
-            if (right - left == p.length() ) {
-    
-                if (hash[s.charAt(left)] >= 0) {
-                    count++;
+            
+            if(end - start == plength){
+                if(arr[s.charAt(start) - 'a'] >= 0){//>=0 means the left char is in p
+                    matched --;
                 }
-                hash[s.charAt(left)]++;
-                left++;    
-            }    
+                arr[s.charAt(start) - 'a'] ++;
+                start ++;//narrow the window by moving start to right
+            }
         }
-        return list;
+        return ans;
     }
 }
