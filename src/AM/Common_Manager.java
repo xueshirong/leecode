fc5b860a91175809b15330f6b317a678379d0a79
xueshr <xueshirong@gmail.com>
2017-05-30 19:28:45 -0500

    update

--- src/AM/Common_Manager.java
+++ src/AM/Common_Manager.java
@@ -1,10 +1,6 @@
 package AM;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Common_Manager {
 	public static void main(String[] args) {
