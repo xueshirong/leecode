0c2a4047b7196ee19e9a42e95bbe888a0bba876c
Xue <Xue@10.10.11.55>
2017-07-07 22:50:05 -0700

    update

--- src/AM/Find_All_Anagrams.java
+++ src/AM/Find_All_Anagrams.java
@@ -7,18 +7,19 @@ public class Find_All_Anagrams {
 		List<Integer> res = findAnagrams("cbaebabacd", "abc");
 		System.out.print(res);
 	}
+	
 	public static List<Integer> findAnagrams(String s, String p) {
 		List<Integer> ans = new ArrayList <Integer>();
 		if (s == null || s.length() == 0 || p == null || p.length() == 0) 
 			return ans;
 		//record each character in p to array
-        int[] arr = new int[30];
-
+        int[] arr = new int[128];// Because the input string is casesenstive, so the size is 128
+        char c = 'A';
         int plength = p.length();
         int slength = s.length();
         
-        for(char c : p.toCharArray()){
-        	arr[c - 'a'] ++;
+        for(char temp : p.toCharArray()){
+        	arr[temp - c] ++;
         }
         //two points
         int start = 0;
@@ -26,11 +27,11 @@ public class Find_All_Anagrams {
         int matched = 0;
         while(end < slength){
         	//if char is in p, then matched increase 1
-            if(arr[s.charAt(end) - 'a'] >= 1){
+            if(arr[s.charAt(end) - c] >= 1){
                 matched ++;
             }
             
-            arr[s.charAt(end) - 'a'] --;
+            arr[s.charAt(end) - c] --;
             end ++;//move right every time
             
             if(matched == plength) {//get the correct anagram
@@ -38,10 +39,10 @@ public class Find_All_Anagrams {
             }
             
             if(end - start == plength){
-                if(arr[s.charAt(start) - 'a'] >= 0){//>=0 means the left char is in p
+                if(arr[s.charAt(start) - c] >= 0){//>=0 means the left char is in p
                     matched --;
                 }
-                arr[s.charAt(start) - 'a'] ++;
+                arr[s.charAt(start) - c] ++;
                 start ++;//narrow the window by moving start to right
             }
         }
