f56bc3629a7798262b6da6a28f6bd239e9dee808
Xue <Xue@Diabolo>
2017-09-11 02:04:49 -0700

    update

--- src/Yelp/Anagram_Checker.java
+++ src/Yelp/Anagram_Checker.java
@@ -4,18 +4,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Anagram_Checker {
 
 	public static void main(String[] args) {
 		String[] strs = {"tea", "eta", "eat", "gab"};
-		List<String> list = anagrams(strs);
+		List<String> list = groupAnagrams(strs);
 		for(String str : list){
 			System.out.println(str);
 		}
 	}
 	
-	public static List<String> anagrams(String[] strs) {
+	public static List<String> groupAnagrams(String[] strs) {
 	    ArrayList<String> result = null;
 	    if(strs == null || strs.length == 0)
 	        return result;
@@ -36,7 +37,7 @@ public class Anagram_Checker {
 	        result = new ArrayList<String>();
 	        if(l.size() >= 1){
 	            for(Integer i: l){
-	                result.add(strs[l]);
+	                result.add(strs[i]);
 
 	            }
 	            System.out.println(result);
@@ -45,20 +46,27 @@ public class Anagram_Checker {
 	    return result;
 	}
 
-	
-	public static boolean isAnagram(String s, String t) {
-        int[] res = new int[256];
-        for (int i = 0; i < s.length(); i++){
-            res[s.charAt(i) - 'a'] = res[s.charAt(i) - 'a'] + 1;
+	public static List<List<String>> groupAnagrams1(String[] strs) {
+        //将所有字符串排序，排序后的结果作为key，原串作为value，将value放到map中对应的list里。
+        List<List<String>> res = new ArrayList<List<String>>();
+        //corner case
+        if (strs == null || strs.length == 0)
+            return res;
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String s : strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if (!map.containsKey(key)){
+                map.put(key, new ArrayList<String>());
+            }
+            map.get(key).add(s);
         }
-        for (int i = 0; i < t.length(); i++){
-            res[t.charAt(i) - 'a'] = res[t.charAt(i) - 'a'] - 1;
+        //将map中所有的list取出来
+        for (String key : map.keySet()){
+            res.add(map.get(key));
         }
-        for (int i : res){
-            if (i != 0)
-                return false;
-        }
-        return true;
+        return res;
     }
 
 
