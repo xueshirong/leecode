4258effeeee476a8f2ebce941a96073dcd592a74
Xue <Xue@Diabolo>
2017-09-18 01:17:30 -0700

    new

---
+++ src/org/leecode/Test/Sort_Characters_By_FrequencyTest.java
@@ -0,0 +1,13 @@
+package org.leecode.Test;
+
+import org.junit.Test;
+import org.leecode.Sort_Characters_By_Frequency;
+
+public class Sort_Characters_By_FrequencyTest {
+	Sort_Characters_By_Frequency p = new Sort_Characters_By_Frequency();
+	@Test
+	public void testFrequencySort() {
+		p.frequencySort("tree");
+	}
+
+}
